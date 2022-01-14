232bf61cf4b661858990a6fbb1c4c9b69a696307
Md Fuadul Islam Redoy <67591529+redoy1102@users.noreply.github.com>
2022-01-14 18:15:21 +0600

    Create 1132.cpp

---
+++ 1132.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int X, Y, sum=0;
+    
+    cin>>X>>Y;
+    
+    if(X > Y){
+            for(int i = Y; i<= X; i++){
+                if(i % 13 != 0){
+                    sum += i;
+                }
+            }
+        cout<<sum<<endl;
+    }
+    else if(Y > X){
+        for(int i = X; i<= Y; i++){
+                if(i % 13 != 0){
+                    sum += i;
+                }
+            }
+        cout<<sum<<endl;
+    }
+    
+    return 0;
+}
