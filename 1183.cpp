2814b5f37dd7f94079307084b8f6fa4ef37c047e
Md Fuadul Islam Redoy <67591529+redoy1102@users.noreply.github.com>
2022-01-28 01:44:59 +0600

    Create 1183.cpp

---
+++ 1183.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char O;
+    double M[12][12], sum = 0, c=0;
+    cin>>O;
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            cin>>M[i][j];
+        }
+    }
+    for(int k = 0; k < 11; k++){
+        for(int l = 1+k; l < 12; l++){
+            sum += M[k][l];
+            c++;
+        }
+    }
+    if(O == 'S'){
+        cout<<fixed<<setprecision(1)<<sum<<endl;
+    }
+    else if(O == 'M'){
+        cout<<fixed<<setprecision(1)<<(sum/c)<<endl;
+    }
+    return 0;
+}
