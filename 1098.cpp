d9b94f706ff4bc54e7b631629ea6a160b56588a3
Md Fuadul Islam Redoy <67591529+redoy1102@users.noreply.github.com>
2022-01-31 13:51:57 +0600

    Create 1098.cpp

---
+++ 1098.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float i = 0, j = 1;
+
+    for(i; i <= 2.1; i = i + 0.2){
+        cout<<"I="<<i<< " J="<<(j+i)<<endl;
+        cout<<"I="<<i<< " J="<<(j + 1 + i)<<endl;
+        cout<<"I="<<i<< " J="<<(j + 2 + i)<<endl;
+    }
+    return 0;
+}
