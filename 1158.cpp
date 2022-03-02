c9611b4f9f1682a6732c8a56d59e71735caf0456
Md Fuadul Islam Redoy <67591529+redoy1102@users.noreply.github.com>
2022-03-02 23:35:28 +0600

    Update 1158.cpp
    Logic changed

--- 1158.cpp
+++ 1158.cpp
@@ -1,23 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int N, X, Y, sum = 0, C = 1;
-    cin>>N;
-    for(int i = 1; i <= N; i++){
-        cin>>X>>Y;
-        if(X %2 == 0){
-            X++;
-            for(int j = 0; j < Y; j++, X+=2){
-                sum += X;
+#include <stdio.h>
+void main()
+{
+    int N, x, y, sum, c;
+    sum = 0; c = 0;
+
+    scanf("%d",&N);
+    
+    while(N--){
+        scanf("%d%d",&x, &y);
+        while(1){
+            if(x %2 != 0)
+            {
+                sum += x;
+                c++;
+                if(c == y)
+                {
+                    printf("%d\n",sum);
+                    sum = 0;
+                    c = 0;
+                    break;
+                }
             }
+            x++;
         }
-        else if(X %2 != 0){
-            for(int k = 0; k < Y; k++, X+=2){
-                sum += X;
-            }
-        }
-        cout<<sum<<"\n";
-        sum = 0;
     }
-    return 0;
 }
